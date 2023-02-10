429d7d8e8cb46ceaaf5b8d9d4fd95757e676be1f
Hyunjun Jang <request10hour@gmail.com>
2023-02-10 10:23:57 +0900

    PWA setting

--- japanese-word/src/main.js
+++ japanese-word/src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VuePwaInstaller from 'vue-pwa-installer'
 
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
+
+Vue.use(VuePwaInstaller)
