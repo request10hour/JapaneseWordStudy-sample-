0a43baeb8a2683e4a278ef76248ba4db9c4c716f
Hyunjun Jang <request10hour@gmail.com>
2023-02-28 12:57:12 +0900

    WebHistory -> WebHashHistory

--- japanese-word/src/router/index.js
+++ japanese-word/src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import MainPage from '@/components/MainPage.vue'
 import DifficultyPage from '@/components/DifficultyPage.vue'
 import QuizPage from '@/components/QuizPage.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', component: MainPage },
     { path: '/difficulty', component: DifficultyPage },
